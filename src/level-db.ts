a572fb120f0ee3389a9ea7a9b1fd6f239557d760
wowbat <weiwei3381@live.com>
2022-01-02 15:55:39 +0800

    feature: 提高搜索性能, 使用levelDB这种数据库, 代替json文件, 同时修复了搜索bug

---
+++ src/level-db.ts
@@ -0,0 +1,52 @@
+/**
+ * 测试levelDB存储倒排索引
+ */
+
+import level from 'level';
+import { config } from './config';
+import { json_to_map, map_to_json } from './utils';
+
+const db: level.LevelDB<string, any> = level(config.levelDb_path);
+
+let save_num = 0;
+async function save_map(
+  key: string,
+  value: Map<string | number, unknown>,
+): Promise<void> {
+  const json_value: string = map_to_json(value);
+  try {
+    await db.put(key, json_value);
+    save_num += 1;
+    if (save_num % 1000 === 0) console.log(save_num);
+  } catch (error) {
+    console.log(`save [key = ${key}] error: ${error}`);
+  }
+}
+
+async function query_map(key: string): Promise<Map<string, any>> {
+  try {
+    const map_json: string = await db.get(key);
+    const map_value: Map<string, any> = json_to_map(map_json);
+    return map_value;
+  } catch (error) {
+    console.log(`query [key = ${key}] error: ${error}`);
+  }
+}
+
+/**
+ * 构建levelDB
+ */
+async function build() {
+  const reverse_dict = new Map();
+  for (const key of reverse_dict.keys()) {
+    const sentence_id_map = reverse_dict.get(key);
+    await save_map(key, sentence_id_map);
+  }
+}
+
+async function get(key: string): Promise<Map<any, any>> {
+  const result_map = await query_map(key);
+  return result_map;
+}
+
+export const reverse_dict = { get };
--- src/test-search.ts
+++ src/test-search.ts
@@ -4,7 +4,22 @@
 
 import { split_words } from './utils';
 import { cloneDeep } from 'lodash';
-import { reverse_dict, get_sentence_by_id } from './db';
+import { reverse_dict } from './level-db';
+import fs from 'fs';
+import { config } from './config';
+
+const sentence_list_txt = fs.readFileSync(config.sentences_db, {
+  encoding: 'utf-8',
+});
+export const sentence_list: string[] = JSON.parse(sentence_list_txt);
+/**
+ * 根据句子id返回指定的句子
+ * @param id 句子id
+ * @returns 句子
+ */
+export function get_sentence_by_id(id: number): string {
+  return sentence_list[id];
+}
 
 /**
  * 搜索部分,分为连续部分和离散部分
@@ -26,54 +41,77 @@ function split_search_value(search_value: string): SearchContinueParts {
   return search_continue_parts;
 }
 
+async function search_continue_part(
+  continue_part: string[],
+): Promise<Map<number, number[]>> {
+  // 获取第一个词元对应的结果, 将以这个结果不断缩减
+  const init_map = await reverse_dict.get(continue_part[0]);
+  const result_sentence_map: Map<number, number[]> = cloneDeep(init_map);
+
+  for (let i = 1; i < continue_part.length; i++) {
+    const search_element: string = continue_part[i];
+    const search_id_map = await reverse_dict.get(search_element);
+
+    // 判断连续情况
+    for (const sentence_id of result_sentence_map.keys()) {
+      const compare_pos: number[] = [];
+      if (search_id_map.has(sentence_id)) {
+        // 获得前一个词的位置列表, 以及后一个词的位置列表, 如果前一个词的位置中含有后一个词的位置-1, 则代表两者相邻
+        const previous_pos_list = result_sentence_map.get(sentence_id);
+        const next_pos_list = search_id_map.get(sentence_id);
+        for (const pos of next_pos_list) {
+          if (previous_pos_list.includes(pos - 1)) compare_pos.push(pos);
+        }
+      }
+      //如果结果集合中没有该句子id, 则删除
+      if (compare_pos.length > 0) {
+        result_sentence_map.set(sentence_id, compare_pos);
+      } else {
+        result_sentence_map.delete(sentence_id);
+      }
+    }
+  }
+  return result_sentence_map;
+}
+
 /**
  * 搜索得到对应的结果, 根据连续和空格进行区分, 例如搜索关键词为"世界的本源 思想",
  * 就搜索文章中含有"世界的本源"和"思想"的所有文本
  * @param search_value 搜索关键词
  */
-function search(search_value: string): void {
-  // 获取第一个词元对应的结果, 将以这个结果不断缩减
+async function search(search_value: string): Promise<Map<number, number[]>> {
   const search_continue_parts = split_search_value(search_value); // 获取连续搜索词元部分
+  // 如果没有搜索词, 则返回为空
+  if (search_continue_parts.length === 0) {
+    console.info('请输入搜索关键词');
+    return new Map();
+  }
+
+  // 如果搜索词只有一个连续长度, 直接返回即可
+  if (search_continue_parts.length === 1) {
+    const continue_result = await search_continue_part(
+      search_continue_parts[0],
+    );
+    return continue_result;
+  }
+
+  // 多个连续部分进行求交集
+  const continue_result_maps: Array<Map<number, number[]>> = []; // 连续部分结果集合
+  for (const continue_part of search_continue_parts) {
+    const continue_result = await search_continue_part(continue_part);
+    continue_result_maps.push(continue_result);
+  }
+  // 获取第一个词元对应的结果, 将以这个结果不断缩减
   const result_sentence_map: Map<number, number[]> = cloneDeep(
-    reverse_dict.get(search_continue_parts[0][0]),
+    continue_result_maps[0],
   );
-
-  for (let i = 0; i < search_continue_parts.length; i++) {
-    const search_part: string[] = search_continue_parts[i];
-    for (let j = 0; j < search_part.length; j++) {
-      const search_element: string = search_part[j];
-      const search_id_map = reverse_dict.get(search_element);
-
-      // 每个连续词元列表的第一个元素不做位置判断, 只做是否有的判断
-      if (j === 0) {
-        if (i === 0) continue; // 如果是第一个词元列表的第一个元素则跳过
-        for (const sentence_id of result_sentence_map.keys()) {
-          if (!search_id_map.has(sentence_id))
-            result_sentence_map.delete(sentence_id);
-        }
-        continue;
-      }
-      // 判断连续情况
-      for (const sentence_id of result_sentence_map.keys()) {
-        const compare_pos: number[] = [];
-        if (search_id_map.has(sentence_id)) {
-          // 获得前一个词的位置列表, 以及后一个词的位置列表, 如果前一个词的位置中含有后一个词的位置-1, 则代表两者相邻
-          const previous_pos_list = result_sentence_map.get(sentence_id);
-          const next_pos_list = search_id_map.get(sentence_id);
-          for (const pos of next_pos_list) {
-            if (previous_pos_list.includes(pos - 1)) compare_pos.push(pos);
-          }
-        }
-        //如果结果集合中没有该句子id, 则删除
-        if (compare_pos.length > 0) {
-          result_sentence_map.set(sentence_id, compare_pos);
-        } else {
-          result_sentence_map.delete(sentence_id);
-        }
-      }
+  for (let i = 1; i < continue_result_maps.length; i++) {
+    const comp_map = continue_result_maps[i];
+    for (const result_key of result_sentence_map.keys()) {
+      if (!comp_map.has(result_key)) result_sentence_map.delete(result_key);
     }
   }
-  print_result(result_sentence_map); // 打印结果
+  return result_sentence_map;
 }
 
 /**
@@ -87,5 +125,16 @@ function print_result(result_map: Map<number, number[]>): void {
   }
 }
 
-const search_sentence = '世界的本源 思想'; // 搜索短句
-console.log(search(search_sentence));
+async function main() {
+  print_result(await search('人道主义困难'));
+  console.log('==============================================================');
+  print_result(await search('全方位 全覆盖的民主'));
+  console.log('==============================================================');
+  print_result(await search('重大风险 经济工作会议'));
+  console.log('==============================================================');
+  print_result(await search('中国疫苗 热烈欢迎'));
+  console.log('==============================================================');
+  print_result(await search('中国共产党和中国人民 发展道路'));
+}
+
+main();
